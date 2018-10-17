7e5da17ae36c7f3ddc56edf4e1d2761f8d3ed5f0
nolash <dev@holbrook.no>
2018-10-17 16:11:13 +0200

    Extract topic function

--- swarm/mutable-resources/client-digest/topic.js
+++ swarm/mutable-resources/client-digest/topic.js
@@ -2,21 +2,7 @@ var web3 = require("web3");
 
 zeros = "0000000000000000000000000000000000000000000000000000000000000000";
 
-function createManifestFromHex(topic, name, user, epoch, level) {
-
-	// check user address param sanity
-	if (!web3.utils.isHex(user)) {
-		throw "user must be hex";
-		return "";
-	}
-	if (user.substring(0, 2) != "0x") {
-		user = "0x"+user;
-	}
-	if (user.length != 42) {
-		throw "user must be 20 byte ethereum address";
-		return "";
-	}
-
+function getFeedTopic(topic, name) {
 	// check topic sanity
 	if (!web3.utils.isHex(topic)) {
 		throw "topic must be hex";
@@ -36,7 +22,6 @@ function createManifestFromHex(topic, name, user, epoch, level) {
 	var topicPadded = zeros + topic;
 	topic = topicPadded.slice(-64);
 
-
 	var topicXored = "";
 	var nameLimit;
 	if (name.length > 32) {
@@ -51,11 +36,28 @@ function createManifestFromHex(topic, name, user, epoch, level) {
 		}
 		var topicByte = parseInt(topic.substring(i*2, (i*2)+2), 16);
 		topicXored += ("0"+(nameByte ^ topicByte).toString(16)).slice(-2);
-		//console.log("name " + nameByte + " topic " + topicByte + " = " + (nameByte ^ topicByte));
+	}
+	
+	return topicXored;
+}
+
+function getFeedManifestFromHex(topic, name, user) {
+
+	// check user address param sanity
+	if (!web3.utils.isHex(user)) {
+		throw "user must be hex";
+		return "";
+	}
+	if (user.substring(0, 2) != "0x") {
+		user = "0x"+user;
+	}
+	if (user.length != 42) {
+		throw "user must be 20 byte ethereum address";
+		return "";
 	}
 
-	return '{"entries":[{"contentType":"application/bzz-feed","mod_time":"0001-01-01T00:00:00Z","feed":{"topic":"0x' + topicXored + '","user":"' + user + '"}}]}';
+	return '{"entries":[{"contentType":"application/bzz-feed","mod_time":"0001-01-01T00:00:00Z","feed":{"topic":"0x' + getFeedTopic(topic, name) + '","user":"' + user + '"}}]}';
 
 }
 
-console.log(createManifestFromHex("0x660000000000000000000000000000000000000000000000000000000000002a", "foo", "19cb96e2fcf9afd95ef06a504ca4feb89c05ca88"));
+console.log(getFeedManifestFromHex("0x660000000000000000000000000000000000000000000000000000000000002a", "foo", "19cb96e2fcf9afd95ef06a504ca4feb89c05ca88"));
