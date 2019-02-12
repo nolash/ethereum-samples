a548fbc50f3e4905a4c7aeb9a78bc1efdafc4920
nolash <dev@holbrook.no>
2019-02-12 12:07:55 +0100

    Remove old start from jaeger script

--- swarm/jaeger/jaeger-test.sh
+++ swarm/jaeger/jaeger-test.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
 case $1 in
-	"start_old")
-		docker network create --internal --subnet 10.1.2.0/24 swarminternal
-		docker run -d --ip 10.1.2.2 --network swarminternal --rm --name jaeger -e COLLECTOR_ZIPKIN_HTTP_PORT=9411 -p 16686:16686 jaegertracing/all-in-one:1.8
-		for i in {1..4}; do
-			docker run -d --ip 10.1.2.1$i --network swarminternal --rm -e PATH=/:/bin:/usr/bin:/usr/local/bin -e PASSWORD=tralala --name bzz$i -h bz$i swarm:trace --tracing --tracing.endpoint 10.1.2.2:6831 --sync-update-delay 1s --bzzport 80 --httpaddr 0.0.0.0 --tracing.svc "bz$i"
-		done
-		# wait for nodes to start
-		sleep 10
-		ENODE=`docker exec bzz1 /geth attach /root/.ethereum/bzzd.ipc --exec admin.nodeInfo.enode`
-		ENODENEW=`echo -n $ENODE | sed -e "s/^\"\(.*\)@127.0.0.1:\([0-9]*\)?.*$/\1@10.1.2.11:\2/g"`
-		echo "b$i: '$ENODE'"
-		echo "a$i: '$ENODENEW'"
-		for i in {2..4}; do
-			docker exec bzz$i /geth attach /root/.ethereum/bzzd.ipc --exec "admin.addPeer('$ENODENEW')"
-			sleep 1
-			docker exec bzz$i /geth attach /root/.ethereum/bzzd.ipc --exec admin.peers
-		done 
-		;;
 	"start")
 		docker network create --internal --subnet 10.1.2.0/24 swarminternal
 		docker run -d --ip 10.1.2.2 --network swarminternal --rm --name jaeger -e COLLECTOR_ZIPKIN_HTTP_PORT=9411 -p 16686:16686 jaegertracing/all-in-one:1.8
